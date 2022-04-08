cc6e2b3674ef3adb590a4a4043aa70b1cfc6e082
unknown <boutaazineb08@gmail.com>
2022-04-08 22:47:18 +0100

    :octocat: first commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Interactive comenting section
---
+++ pages/index.js
@@ -0,0 +1,6 @@
+// import Head from 'next/head';
+// import Image from 'next/image';
+
+export default function Home() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
