c4d15b9c0fc14832efe291089357927d16da733c
unknown <boutaazineb08@gmail.com>
2022-04-13 22:05:40 +0100

    Set firebase & implement Auth.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Interactive comenting section
+
+
+Adding commentend it is really difficult for static sites, this is an easy way to add commenting section to a static
+people being able.
+this probably would an eay saas solution that your cann static comments to your site.
+
+
+main concept that's being used here, is taking the advantage from the ISR(Incremental satatic regenaration ) provided by nextjs 
+which periodically regenarates the static page in the packground. This
+
+one of the things that we take into a consideration before building any commenting system in the spam comment, to enforce no spam  people here will login with there github or gmail. so there is a name put there plus, in the back side you will have moderation 
+so we can remove comments that are not necessar.
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,41 @@
 // import Head from 'next/head';
 // import Image from 'next/image';
+import { useAuth } from '../lib/auth';
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const auth = useAuth();
+  if (!auth.user) {
+    return (
+      <>
+        <button
+          className="border-2 border-sky-500 rounded-full p-2 hover:bg-sky-500 hover:text-white"
+          onClick={(e) => {
+            auth.signinWithGitHub();
+          }}
+        >
+          SignIn with github
+        </button>
+        <button
+          className="border-2 border-sky-500 rounded-full p-2 hover:bg-sky-500 hover:text-white"
+          onClick={(e) => {
+            auth.signinWithGoogle();
+          }}
+        >
+          SignIn with Google
+        </button>
+      </>
+    );
+  }
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button
+        onClick={(e) => {
+          auth.signout();
+        }}
+      >
+        sign out
+      </button>
+    </>
+  );
 }
